4243e69ac7b3d0c142dcb776de9078a5c0466c35
razanalk <13238holbeertonschool@gmail.com>
2026-09-06 15:16:35 +0000

    Fix simple pagination validation

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -5,25 +5,25 @@ import csv
 import math
 from typing import List
 
+
 def index_range(page: int, page_size: int) -> tuple:
-    """Returns a tuple containing the start\
-        and end indices for pagination."""
-    first_index = (page - 1) * page_size
-    last_index = first_index + page_size
-    return first_index, last_index
+    """Return the start and end indexes for a pagination range."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize the server."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Return the cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -32,16 +32,12 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Returns a list of rows from the dataset\
-            for the specified page and page size."""
+    def get_page(self, page: int = 1,
+                 page_size: int = 10) -> List[List]:
+        """Return the requested page of the dataset."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
-        data = self.dataset()
 
-        if start_index >= len(data):
-            return []
-
-        return data[start_index:end_index]
+        return self.dataset()[start_index:end_index]
