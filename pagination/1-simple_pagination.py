c478df835532f280eec714b5ad3caf613fc56db6
razanalk <13238holbeertonschool@gmail.com>
2026-09-06 15:12:29 +0000

    Add simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -5,25 +5,25 @@ import csv
 import math
 from typing import List
 
-
 def index_range(page: int, page_size: int) -> tuple:
-    """Return the start and end indexes for a pagination range."""
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return (start_index, end_index)
+    """Returns a tuple containing the start\
+        and end indices for pagination."""
+    first_index = (page - 1) * page_size
+    last_index = first_index + page_size
+    return first_index, last_index
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initialize the server with an empty dataset cache."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Return the cached dataset."""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -32,12 +32,16 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
-        """Return the requested page of the dataset."""
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a list of rows from the dataset\
+            for the specified page and page size."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
 
-        return self.dataset()[start_index:end_index]
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
