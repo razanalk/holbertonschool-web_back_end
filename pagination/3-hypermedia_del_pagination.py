6654e8d557a1d4909b5b426f61fc24d6abb8a158
razanalk <13238holbeertonschool@gmail.com>
2026-09-07 14:06:33 +0000

    Add deletion resilient pagination

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import Dict, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Return the cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Return the dataset indexed by its original positions."""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None,
+                        page_size: int = 10) -> Dict:
+        """Return a page while skipping deleted rows."""
+        if index is None:
+            index = 0
+
+        indexed_data = self.indexed_dataset()
+        dataset_size = len(self.dataset())
+
+        assert type(index) is int and 0 <= index < dataset_size
+        assert type(page_size) is int and page_size > 0
+
+        data = []
+        next_index = index
+
+        while len(data) < page_size and next_index < dataset_size:
+            if next_index in indexed_data:
+                data.append(indexed_data[next_index])
+            next_index += 1
+
+        return {
+            "index": index,
+            "data": data,
+            "page_size": len(data),
+            "next_index": next_index
+        }
